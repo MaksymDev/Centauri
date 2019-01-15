b471230786fa4e4aca590c4ac60590cd596bbb8e
Maks <>
2019-01-15 16:43:29 -0700

    Cenrauri Upload Personal

---
+++ wp-content/plugins/bb-plugin/includes/row.php
@@ -0,0 +1,15 @@
+<?php $element = ( ! empty( $row->settings->container_element ) ? $row->settings->container_element : 'div' ); ?>
+<<?php echo $element; ?><?php FLBuilder::render_row_attributes( $row ); ?>>
+	<div class="fl-row-content-wrap">
+		<?php FLBuilder::render_row_bg( $row ); ?>
+		<?php do_action( 'fl_builder_render_node_layers', $row ) ?>
+		<div class="<?php FLBuilder::render_row_content_class( $row ); ?>">
+		<?php
+		// $groups received as a magic variable from template loading.
+		foreach ( $groups as $group ) {
+			FLBuilder::render_column_group( $group );
+		}
+		?>
+		</div>
+	</div>
+</<?php echo $element; ?>>
